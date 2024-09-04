c584d6e584e8f412c564129d4b8d877b59947cb2
Terje Sandstrom <terje@hermit.no>
2024-09-04 10:30:46 +0200

    Initial

---
+++ MyWebApp/MyWebApp.Server/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseSpa(spa =>
+{
+    // To learn more about options for serving an Angular SPA from ASP.NET Core,
+    // see https://go.microsoft.com/fwlink/?linkid=864501
+
+    spa.Options.SourcePath = "ClientApp/";
+    spa.Options.StartupTimeout = new TimeSpan(0, 5, 0);
+    spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions()
+    {
+        OnPrepareResponse = ctx =>
+        {
+            ctx.Context.Response.Headers.CacheControl = "no-cache, no-store";
+            ctx.Context.Response.Headers.Pragma = "no-cache";
+            ctx.Context.Response.Headers.Expires = "-1";
+        }
+    };
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseAngularCliServer("start");
+    }
+});
+app.Run();
